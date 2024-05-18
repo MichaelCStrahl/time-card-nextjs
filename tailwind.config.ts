f95e2ed39d36d3acdacebc6f3a96f9c4c5866b5f
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-05-18 17:35:29 -0300

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		fontFamily: {
+			sans: "var(--font-montserrat)",
+		},
+		extend: {
+			colors: {
+				blue: {
+					1000: "#1E2733",
+					1200: "#151F2B",
+				},
+				orange: { 450: "#FE8A00" },
+				neutral: { 450: "#CFCFCF" },
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
